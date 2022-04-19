2faeccf3b71f3d095ca92b7eb02cce88782c02b0
sunnyjis49 <84003278+sunnyjis49@users.noreply.github.com>
2022-04-19 17:09:55 +0530

    google cloud poc

--- CoreJavaPractice/src/test.java
+++ CoreJavaPractice/src/test.java
@@ -1,5 +1,8 @@
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 
@@ -15,11 +18,25 @@ public class test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		LocalTime localtime = LocalTime.now();
-		System.out.println("localtime: "+localtime.getMinute());
+		/*
+		 * LocalTime localtime = LocalTime.now();
+		 * System.out.println("localtime: "+localtime.getMinute());
+		 * 
+		 * LocalDateTime localdatetime = LocalDateTime.now();
+		 * System.out.println("localdatetime: "+localdatetime);
+		 */
 		
-		LocalDateTime localdatetime = LocalDateTime.now();
-		System.out.println("localdatetime: "+localdatetime);
+		List<Integer> list1 = new LinkedList<>(Arrays.asList(9,9,9,9,9));
+		List<Integer> list2 = new LinkedList<>(Arrays.asList(9,9,9,0,0));
+		List<Integer> list3 = new LinkedList<>();
+		int carry = 0;
+		for(int i=0; i<list1.size(); i++) {
+			int sum = list1.get(i)+list2.get(i)+carry;
+			list3.add((sum)%10);
+			carry=sum/10;
+		}
+		
+		list3.forEach(System.out::println);
 
 	}
 
